9bbdf4454fdda6ecbe572f56d91fa934e3b9b959
nockcha11 <lie8220@naver.com>
2024-06-03 08:53:47 +0900

    ✨ 팝업창, 모바일사이드바 클릭이벤트, 매개변수

--- box-layout/position/실습1/script.js
+++ box-layout/position/실습1/script.js
@@ -5,25 +5,25 @@
 
 // // function() {}
 
-// // popupOpenBtn 클릭시 popup에 is active class Add
-// popupOpenBtn.addEventListener('click', function(){
-//   // classList.add('클래스명')
-//   popup.classList.add('is-active')
-// })
+// popupOpenBtn 클릭시 popup에 is active class Add
+popupOpenBtn.addEventListener('click', function(){
+  // classList.add('클래스명')
+  popup.classList.add('is-active')
+})
 
-// popupCloseBtn.addEventListener('click', function(){
-//   popup.classList.remove('is-active');
-// })
+popupCloseBtn.addEventListener('click', function(){
+  popup.classList.remove('is-active');
+})
 
 
 
-// // sidebar
-// let sidebar = document.querySelector('.sidebar');
-// let sidebarOpenBtn = document.querySelector('.sidebar-open-btn');
-// let sidebarCloseBtn = document.querySelector('.sidebar-close-btn');
+// sidebar
+let sidebar = document.querySelector('.sidebar');
+let sidebarOpenBtn = document.querySelector('.sidebar-open-btn');
+let sidebarCloseBtn = document.querySelector('.sidebar-close-btn');
 
-// //콘솔로그에 변수명선언 여러개 (-,-,-,)
-// console.log(sidebar, sidebarOpenBtn, sidebarCloseBtn )
+//콘솔로그에 변수명선언 여러개 (-,-,-,)
+console.log(sidebar, sidebarOpenBtn, sidebarCloseBtn )
 
 // //1. sidebarOpenBtn을 클릭 했을 때
 // // sidebar에 is-active 클래스 추가
@@ -34,12 +34,26 @@
 //   sidebar.classList.add('is-active');
 // })
 
-// // sidebarCloseBtn 클릭했ㅇ르때, sidebar에 is-active 믈래스 제거
+// // sidebarCloseBtn 클릭했을때, sidebar에 is-active 클래스 제거
 // sidebarCloseBtn.addEventListener('click',function(){
 //   sidebar.classList.remove('is-active');
 // })
 
 
+// function 함수명
+
+function openSidebar(){
+  // 1. sidebar 라는 클래스에 is-active 클래스 추가
+  sidebar.classList.add('is-active');
+}
+
+function CloseSidebar(){
+  // 1. sidebar 라는 클래스에 is-active 클래스 제거
+  sidebar.classList.remove('is-active');
+}
+
+sidebarOpenBtn.addEventListener('click', openSidebar);
+sidebarCloseBtn.addEventListener('click', CloseSidebar);
 
 
 // 함수 소괄호 안 : 매개변수
@@ -51,3 +65,5 @@ function plus (num1, num2) {
 
 // 함수는 호출 했을 때 실행된다
 plus(100,10);
+
+
