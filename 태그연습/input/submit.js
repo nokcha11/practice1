65399169d0d0ee67ae73607632276cbfc23248a0
nokcha11 <lie8220@naver.com>
2024-06-10 03:47:44 +0900

    ✨ 생년월일 for문으로select-option태그만들기

--- 태그연습/input/submit.js
+++ 태그연습/input/submit.js
@@ -1,8 +1,60 @@
-let box = document.querySelector('.box');
 
-let txt = `
- <h1>안녕하세요</h1>
- <p>태그 추가</p>
-`
+let year = document.querySelector('.year');
+let month = document.querySelector('.month');
+let date = document.querySelector('.date');
 
-box.insertAdjacentHTML('afterbegin', txt);
+//한국표준시 (Korea Standard time)
+let kst = new Date();
+console.log(kst)
+
+let getYear = kst.getFullYear(); 
+let getMonth = kst.getMonth() + 1; /* 월+1 */
+let getDate = kst.getDate();
+console.log(getYear, getMonth, getDate)
+
+for(let i = 1920; i <= getYear; i++) {
+  let options = `<option>${i}</option>`;
+  year.insertAdjacentHTML('beforeend', options);
+}
+
+ // 월 옵션 추가
+for (let j = 1; j <= 12; j++) {
+  let options = `<option>${j}</option>`;
+  month.insertAdjacentHTML('beforeend', options);
+}
+
+// 일 옵션 추가
+for (let k = 1; k <= 30; k++) {
+  let options = `<option>${k}</option>`;
+  date.insertAdjacentHTML('beforeend', options);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let box = document.querySelector('.box');
+
+// let txt = `
+//  <h1>안녕하세요</h1>
+//  <p>태그 추가</p>
+// `
+
+// box.insertAdjacentHTML('afterbegin', txt);
